b7d08ba6131ab5ae1362b994ea54b76b7597d684
ethrelfall <77395015+ethrelfall@users.noreply.github.com>
2022-08-02 17:54:43 +0100

    Update re-entrant_corner_primal.py

--- re-entrant_corner_primal.py
+++ re-entrant_corner_primal.py
@@ -31,3 +31,8 @@ bc2 = DirichletBC(V.sub(1), 0.0, {16,18,20})  # edges with vertical normals
 solve(a==L,g, bcs=[bc1, bc2])
 
 File("re-entrant_corner_primal.pvd").write(g)
+
+# plot the RHS from evaluating Laplacian of the solution - surprisingly, the equation seems well-satisfied away from the re-entrant corner (NOTE use at least order-2 for this)
+r = Function(V)
+r.interpolate(grad(div(g))-curl(curl(g)))
+File("re-entrant_corner_primal_rhs.pvd").write(r)
